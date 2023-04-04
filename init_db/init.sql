2bfc41f34c08c2b80aab5ae5cadf0a5e06157830
yut_fut <seregey.golubev@mail.ru>
2023-04-04 03:37:46 +0300

    edited init db sql

--- init_db/init.sql
+++ init_db/init.sql
@@ -1,24 +1,33 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS tasks CASCADE;
+
 SET timezone TO '+03';
 
-create table task
+create table tasks
 (
     id SERIAL PRIMARY KEY,
-    name text,
+    name VARCHAR(256) NOT NULL,
     description text,
     public_tests text[],
     private_tests text[],
     generated_tests text[],
     difficulty text,
-    cf_contest_id text,
-    cf_index text,
-    cf_points text,
-    cf_rating text,
-    cf_tags text,
+    cf_contest_id int,
+    cf_index VARCHAR(3),
+    cf_points float,
+    cf_rating int,
+    cf_tags text[],
     time_limit text,
-    memory_limit_bytes text,
-    link text,
+    memory_limit_bytes int,
+    link VARCHAR(256),
     task_ru text,
     input text,
     output text,
     note text
+);
+
+CREATE TABLE users (
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(256) NOT NULL,
+    password VARCHAR(256) NOT NULL
 );
